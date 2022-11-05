c05b8c03a8c2af0374b00295f2b231268a8f3c23
Student <student@itstep.ge>
2022-11-05 18:50:12 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// function getNum(x){
+//     return x + 10;
+// }
+// let num = getNum(50);
+// console.log(num);
+
+// function getNum2(x){
+//     console.log(x + 10)
+// }
+// getNum2(15)
+
+// let p = document.querySelector("p");
+// let btn = document.querySelector("button");
+
+// function makeRandRgb(){
+//     let r = Math.round(Math.random() * 255)
+//     let g = Math.round(Math.random() * 255)
+//     let b = Math.round(Math.random() * 255)
+    
+//     return `rgb ${r}, ${g}, ${b}`
+// }
+// let rgbColor = makeRandRgb();
+// console.log(rgbColor);
+
+// btn.addEventListener('click',function(){
+//     document.body.style.backgroundColor = makeRandRgb();
+//     p.innerHTML = document.body.style.backgroundColor;
+// })
+
+// function name()[
+
+// ]
+
+// let makeArray = function(){
+//     console.log(`my name is ${firstName}`)
+// }
+// makeArray("Andria");
+
+// let func = (x,y) => x + y
+
+
+// let sum = func(10,20);
+// console.log(sum)
+
+// console.log(this)
+
+let car = {
+    color:"black",
+    year:"2000",
+    engine:"6.3",
+    model:"c"
+}
+console.log(car)
